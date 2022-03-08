4fdaae8d544159606a54f653095ede49c3f44d5a
candipy <88384618+candipy@users.noreply.github.com>
2022-03-08 16:23:23 +0100

    Etape 3.2

--- front/js/script.js
+++ front/js/script.js
@@ -19,17 +19,27 @@ fetch("http://localhost:3000/api/products/")
     //   console.log(i.name)
     // }
     for (i of products){
+
       //Pour chaque tour dans products, on pointe l'ID "items" dans HTML
       document
       .getElementById("items")
       // Puis on ajoute des éléments HTML, ex paragraphes pour tester, j'ai vu que avec juste = il n'y avait qu'un élément affiché, avec += c'est 8, soit le nombre de i
       // .innerHTML += "<p>texte</p>";
 
-    
+    // Après plusieurs tests avec des "" '' des (), j'ai retrouvé ca dans le cours : Pour créer une string interpolation on écrit du texte encadrée par le signe  `  et si on veut injecter une variable dans ce code on utilise l’expression  ${maVariable}. 
+      // Ca n'a fonctionné que quand j'ai copié collé depuis le code commenté dans index.html. quand j'ai tappé avec des tab ou des espaces ou sans ca n'a pas fonctionné 
 
-      .innerHTML += " <a href='./product.html?id=i._id'><article><img src='i.urlImage' alt=i.altTxt/><h3 class='productName'>i.name</h3><p class='productDescription'>i.description</p></article></a>"
-      ;
-    }
+      
+      .innerHTML += `<a href="./product.html?${i._id}">
+      <article>
+        <img src=${i.imageUrl} alt=${i.altTxt} />
+        <h3 class="productName">${i.name}</h3>
+        <p class="productDescription">${i.description}</p>
+      </article>
+    </a>`;
+      
+;
+    };
   })
   .catch(function(err) {
     // Une erreur est survenue
