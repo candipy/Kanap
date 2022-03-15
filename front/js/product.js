0b497ff49ef6cd6854a628f2745635b0b88acb71
candipy <88384618+candipy@users.noreply.github.com>
2022-03-15 17:08:55 +0100

    Etape 6

--- front/js/product.js
+++ front/js/product.js
@@ -1,8 +1,58 @@
-// Il faut maintenant récupérer l'id ayant été cliqué sur la page d'accueil
+// Objectif : Ajouter les éléments du produit cliqué dans la page d'acceuil
+
+
+// Besoins 
+// A. Récupérer l'id id ayant été cliqué sur la page d'accueil
 
 //1) Récupération de l'URL de la page affichée
-var url = new URL(location.href);
+const url = new URL(location.href);
 //2) Recherche dans l'url le paramètre de l'ID
-var idProduit = url.searchParams.get("id");
-console.log(idProduit);
+const idProduct = url.searchParams.get("id");
+// Vérification dans la console
+console.log(idProduct);
+
+// B. Intérroger l'API avec l'ID qui est maintenant présent 
+const idProductAPI = `http://localhost:3000/api/products/${idProduct}` 
+
+
+fetch(idProductAPI)
+.then(function(res) {
+    if (res.ok) {
+      return res.json();
+    }
+  })
+
+  .then((product)=> {
+      console.log(product)
+
+        const image = document.getElementsByClassName("item__img") [0]// Récupère la class item img dans HTML
+        // getElementsByName renvoi à une nodelist et pas un élément, il faut donc indiquer un index soit ici 0
+        image.innerHTML= `<img src="${product.imageUrl}" alt="${product.altTxt}" />`; // Insère l'url de l'image et son alt
+
+        const titlePage = document.getElementsByTagName('title')[0]; // Récupère la balise title dans HTML
+        titlePage.innerText = product.name; // Insère nom du produit trouvé dans l'API
+
+        const title = document.getElementById("title"); // Recupere id Title dans HTML
+        title.innerText = product.name; //Insére le nom du produit trouvé dans l'API
+
+        const price = document.getElementById("price") // Récupère id price dans HTML
+        price.innerText = product.price // Insére le prix trouvé dans l'API 
+
+        const description = document.getElementById("description") // Récupère id descrption dans HTML
+        description.innerText = product.description; // Insére le texte de la description trouvé dans l'API
+
+        let couleur = document.getElementById("colors") // Récup§re id colors dans HTML
+        // Pour chaque couleur dans product, création d'une nouvelle entrée value
+        for (let color of product.colors){
+            // Pour chaque color dans colors de products provennant de l'API
+        console.log(color)
+        // ajouter à couleur (selectionné par id) dans HTML de l'option {color}
+        couleur.innerHTML+= `<option value="${color}">${color}</option>`
+        };
+       
+  })
+
+  .catch(function(err) {
+    // Une erreur est survenue
+  });
 
--- front/js/script.js
+++ front/js/script.js
@@ -1,40 +1,37 @@
+// Objectif : Insérer les produits dans la page d'accueil 
 
+
+// Besoins : 
+// Elements de l'API
+const productsAPI = "http://localhost:3000/api/products/";
+// ID items dans HTML
+const itemsHtml = document.getElementById("items")
 
 //Je créé un fetch pour récupérer les éléments de l'API afin de pouvoir les utiliser
 
 fetch("http://localhost:3000/api/products/") 
+//asynchrone
 //fonction then pour récupérer le résultat de la requete au format json en ayant vérifié que tout s'est bien passé
-//Ce résultat est une promise : la Promise est un objet qui fournit une fonction then qui sera exécutée quand le résultat aura été obtenu, et une fonction catch qui sera appelée s’il y a une erreur qui est survenue lors de la requête   
+//Ce résultat est une promise : 
+// la Promise est un objet qui fournit une fonction then qui sera exécutée quand le résultat aura été obtenu  et une fonction catch qui sera appelée s’il y a une erreur qui est survenue lors de la requête   
 .then(function(res) {
     if (res.ok) {
       return res.json();
     }
   })
 
-// Ce résultat est une promise à résoudre
+// Ce résultat est une promise à résoudre (then) ou si erreur (catch)
   .then(function(products) {
     console.log(products);
-    // Pour chaque tour dans products, afficher le nom - Ok ca a fonctionné
-    // for (i of products) {
-    //   console.log(i.name)
-    // }
-    for (let i of products){
+    // Afficher les produits
 
-      //Pour chaque tour dans products, on pointe l'ID "items" dans HTML
-      document
-      .getElementById("items")
-      // Puis on ajoute des éléments HTML, ex paragraphes pour tester, j'ai vu que avec juste = il n'y avait qu'un élément affiché, avec += c'est 8, soit le nombre de i
-      // .innerHTML += "<p>texte</p>";
-
-    // Après plusieurs tests avec des "" '' des (), j'ai retrouvé ca dans le cours : Pour créer une string interpolation on écrit du texte encadrée par le signe  `  et si on veut injecter une variable dans ce code on utilise l’expression  ${maVariable}. 
-      // Ca n'a fonctionné que quand j'ai copié collé depuis le code commenté dans index.html. quand j'ai tappé avec des tab ou des espaces ou sans ca n'a pas fonctionné 
-
-      
-      .innerHTML += `<a href="./product.html?id=${i._id}">
+    for (let product of products){
+    //Pour chaque "product" dans "products" j'ajoute les éléments HTML ci dessous à l'id items
+      itemsHtml.innerHTML += `<a href="./product.html?id=${product._id}">
       <article>
-        <img src=${i.imageUrl} alt=${i.altTxt} />
-        <h3 class="productName">${i.name}</h3>
-        <p class="productDescription">${i.description}</p>
+        <img src="${product.imageUrl}" alt="${product.altTxt}" />
+        <h3 class="productName">${product.name}</h3>
+        <p class="productDescription">${product.description}</p>
       </article>
     </a>`;
       
