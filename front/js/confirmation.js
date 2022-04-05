67240523f0a85ad6f18f5f86f16db78c8092c738
candipy <88384618+candipy@users.noreply.github.com>
2022-04-05 14:04:01 +0200

    Etape 11

---
+++ front/js/confirmation.js
@@ -0,0 +1,15 @@
+// Objectif : Récupérer l'orderId dans l'URL pour l'afficher sur la page
+
+//Récupération de l'URL de la page affichée
+const url = new URL(location.href);
+
+//Recherche dans l'url le paramètre de l'orderId
+const orderIdURL = url.searchParams.get("id");
+console.log(orderIdURL);
+
+// Cibler l'id dans HTML
+const orderIdHTML = document.getElementById("orderId");
+console.log(orderIdHTML);
+
+// Ajouter l'orderId trouvé dans l'URL et l'afficher sur la page
+orderIdHTML.innerText = orderIdURL;
